1f81147c3f9e6f3b5c5c6f00245b8d933311be3c
BOGDAN-PC <bogdy2992@gmail.com>
2017-03-23 23:18:01 +0200

    front main view

--- resources/views/_partials/front/menu.blade.php
+++ resources/views/_partials/front/menu.blade.php
@@ -1,6 +1,6 @@
 <div class="list-group">
     <a href="{{ route('hospital.dashboard.index') }}" @if(isset($_active_menu) && $_active_menu == 'hospitalDashboard') class="list-group-item active" @else class="list-group-item" @endif> Dashboard </a>
-    <a href="{{ route('hospital.information.index') }}" @if(isset($_active_menu) && $_active_menu == 'information') class="list-group-item active" @else class="list-group-item" @endif> Informții spital </a>
+    <a href="{{ route('hospital.information.index') }}" @if(isset($_active_menu) && $_active_menu == 'information') class="list-group-item active" @else class="list-group-item" @endif> Informații spital </a>
     <a href="{{ route('hospital.hospitalDescription.index') }}" @if(isset($_active_menu) && $_active_menu == 'hospitalDescription') class="list-group-item active" @else class="list-group-item" @endif> Descriere spital </a>
     <a href="#" class="list-group-item"> Cuvant manager </a>
     <a href="#" class="list-group-item"> Specialitati spital </a>
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,7 +1,37 @@
-<?php
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+                    <div class="panel-body">
+                        <div class="col-sm-3">
+                            @include('_partials.front.menu')
+                        </div>
+                        <div class="col-sm-9">
+                            
+                            {!! Notification::showAll() !!}
+                            
+                            @if (count($errors) > 0)
+                                <div class="alert alert-danger alert-dismissable">
+                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">�</button>
+                                    <h4><i class="icon fa fa-ban"></i> <strong>Whoops!</strong> There were some problems with your input.</h4>
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif 
+                            
+                            @yield('right_content')
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
